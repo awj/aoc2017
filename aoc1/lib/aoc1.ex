c261a740d1f6287b135793eb815509fa7c1e7e0f
ajones <ajones1@gmail.com>
2017-12-04 11:57:12 -0700

    Day 1 solution.

--- aoc1/lib/aoc1.ex
+++ aoc1/lib/aoc1.ex
@@ -3,16 +3,50 @@ defmodule Aoc1 do
   Documentation for Aoc1.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Aoc1.hello
-      :world
-
-  """
   def compute(input) do
-    3
+    input
+    |> break
+    |> total
+  end
+
+  def halfway(input) do
+    input
+    |> break
+    |> build_offsets
+    |> Enum.filter(&(elem(&1,0) == elem(&1,1)))
+    |> Enum.map(&(elem(&1,0)))
+    |> Enum.sum
+  end
+ 
+  defp build_offsets(input) do
+    shiftable = input ++ input ++ input
+    len = Kernel.length(input)
+    offset = Enum.drop(shiftable, round(len / 2))
+
+    Enum.zip(shiftable, offset) |> Enum.take(len)
+  end
+
+  defp break(input_string) do
+    input_string
+    |> Integer.parse
+    |> Kernel.elem(0)
+    |> Integer.digits
+  end
+
+  defp total(input) do
+    last  = List.last input
+
+    input
+    |> Enum.reduce([last, []], &(accum(&1, &2)))
+    |> Enum.at(1)
+    |> Enum.sum
+  end
+
+  def accum(num, [acc, rest]) when num == acc do
+    [acc, [(num | rest)]]
+  end
+
+  def accum(num, [acc, rest]) when num != acc do
+    [num, rest]
   end
 end
--- aoc1/test/aoc1_test.exs
+++ aoc1/test/aoc1_test.exs
@@ -8,4 +8,12 @@ defmodule Aoc1Test do
     assert Aoc1.compute("1234") == 0
     assert Aoc1.compute("91212129") == 9
   end
+
+  test "halfway around computations" do
+    assert Aoc1.halfway("1212") == 6
+    assert Aoc1.halfway("1221") == 0
+    assert Aoc1.halfway("123425") == 4
+    assert Aoc1.halfway("123123") == 12
+    assert Aoc1.halfway("12131415") == 4
+  end
 end
