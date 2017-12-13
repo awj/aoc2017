d122c7e4d208f5eb96e22f872225597d6fbc9c31
ajones <ajones1@gmail.com>
2017-12-13 13:13:30 -0700

    Day 9, part 2.

--- aoc9/lib/aoc9.ex
+++ aoc9/lib/aoc9.ex
@@ -3,7 +3,7 @@ defmodule Aoc9 do
   Documentation for Aoc9.
   """
 
-  defstruct points: 0, nesting: 0, in_garbage: false, ignore: false
+  defstruct points: 0, nesting: 0, in_garbage: false, ignore: false, garbage: 0
 
   def score(input) do
     input
@@ -12,6 +12,13 @@ defmodule Aoc9 do
     |> Map.get(:points)
   end
 
+  def garbage(input) do
+    input
+    |> String.graphemes
+    |> Enum.reduce(%Aoc9{}, fn(c, acc) -> parse(c, acc) end)
+    |> Map.get(:garbage)
+  end
+  
   def parse(_, %Aoc9{ignore: true} = state) do
     %{state | ignore: false}
   end
@@ -28,7 +35,7 @@ defmodule Aoc9 do
     %{state | nesting: state.nesting - 1, points: state.points + state.nesting}
   end
 
-  def parse("<", state) do
+  def parse("<", %Aoc9{in_garbage: false} = state) do
     %{state | in_garbage: true}
   end
 
@@ -36,7 +43,11 @@ defmodule Aoc9 do
     %{state | in_garbage: false}
   end
 
-  def parse(_,state) do
+  def parse(_, %Aoc9{in_garbage: true} = state) do
+    %{state | garbage: state.garbage + 1}
+  end
+
+  def parse(_, state) do
     state
   end
 end
--- aoc9/test/aoc9_test.exs
+++ aoc9/test/aoc9_test.exs
@@ -12,4 +12,14 @@ defmodule Aoc9Test do
     assert Aoc9.score("{{<!!>},{<!!>},{<!!>},{<!!>}}") == 9
     assert Aoc9.score("{{<a!>},{<a!>},{<a!>},{<ab>}}") == 3
   end
+
+  test "handles garbage char counting" do
+    assert Aoc9.garbage("<>") == 0
+    assert Aoc9.garbage("<random characters>") == 17
+    assert Aoc9.garbage("<<<<>") == 3
+    assert Aoc9.garbage("<{!>}>") == 2
+    assert Aoc9.garbage("<!!>") == 0
+    assert Aoc9.garbage("<!!!>>") == 0
+    assert Aoc9.garbage("<{o\"i!a,<{i<a>") == 10
+  end
 end
